5adedc1eb1c93205d64aabb9116a8e6dacce54c9
Max Lawver <max@maxisweird.com>
2012-07-11 20:00:29 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Infinity::Application.config.secret_token = '81fa9a96b430725b98147f8181a754a53c4355b63a0be863ae6f744aeca97c6915b9288bcb3c500383a6d9892562a36f1743e26c0bc98b7816da53077960c626'
